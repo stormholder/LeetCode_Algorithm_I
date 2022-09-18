7e711e5f45795090575f2b0f3070b1e1b2dbdc51
stormholder <dantedmc78@gmail.com>
2022-09-18 13:00:45 +0300

    Added 3rd task initial

---
+++ 35_Search_Insert_Position.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace _35_Search_Insert_Position.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
