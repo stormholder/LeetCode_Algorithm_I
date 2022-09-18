0f1b3a2e4d97ca66a148174e18ba9aa3adde6319
stormholder <dantedmc78@gmail.com>
2022-09-18 13:27:57 +0300

    Update: SearchInsert - 2nd try

--- 35_Search_Insert_Position/Program.cs
+++ 35_Search_Insert_Position/Program.cs
@@ -6,15 +6,24 @@ public class Solution
     {
         decimal L = 0;
         decimal R = nums.Length - 1;
-        int result = nums.Length;
+        int result = nums.Length - 1;
 
+        if (nums[result] < target)
+        {
+            return result + 1;
+        }
         while (R - L > 1)
         {
             var m = (int)Math.Ceiling((L + R) / 2);
-            if (nums[m] > target)
+            if (nums[m] == target)
             {
                 result = m;
+                break;
+            }
+            if (nums[m] > target)
+            {
                 R = m - 1;
+                result = m - 1;
             }
             else
             {
