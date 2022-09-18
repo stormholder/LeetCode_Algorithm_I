85eff81ea452b9ca4deff489b5ae1824d4cb5999
stormholder <dantedmc78@gmail.com>
2022-09-18 13:07:17 +0300

    Tast 3 day 1 naive solution

--- 35_Search_Insert_Position.Test/UnitTest1.cs
+++ 35_Search_Insert_Position.Test/UnitTest1.cs
@@ -7,8 +7,30 @@ namespace _35_Search_Insert_Position.Test
         {
         }
 
-        [Test]
-        public void Test1()
+        static object[] TestCases =
+        {
+            new object[]
+            {
+                new int[] { 1,3,5,6 },
+                5,
+                2
+            },
+            new object[]
+            {
+                new int[] { 1,3,5,6 },
+                2,
+                1
+            },
+            new object[]
+            {
+                new int[] { 1,3,5,6 },
+                7,
+                4
+            },
+        };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(int[] nums, int target, int expenced)
         {
             Assert.Pass();
         }
--- 35_Search_Insert_Position/Program.cs
+++ 35_Search_Insert_Position/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SearchInsertPosition;
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        decimal L = 0;
+        decimal R = nums.Length - 1;
+        int result = nums.Length;
+
+        while (R - L > 1)
+        {
+            var m = (int)Math.Ceiling((L + R) / 2);
+            if (nums[m] > target)
+            {
+                result = m;
+                R = m - 1;
+            }
+            else
+            {
+                L = m;
+            }
+        }
+
+        return result;
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
