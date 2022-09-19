77883e490ed91b966cc300e942984931956a6f21
stormholder <dantedmc78@gmail.com>
2022-09-19 18:27:30 +0300

    Add: Unit tests for task 1

--- 977_Squares.Test/UnitTest1.cs
+++ 977_Squares.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Squares;
+
 namespace _977_Squares.Test
 {
     public class Tests
@@ -7,10 +9,27 @@ namespace _977_Squares.Test
         {
         }
 
-        [Test]
-        public void Test1()
+        static object[] TestCases =
         {
-            Assert.Pass();
+            new object[]
+            {
+                new int[] { -4, -1, 0, 3, 10 },
+                new int[] { 0, 1, 9, 16, 100 }
+            },
+            new object[]
+            {
+                new int[] { -7, -3, 2, 3, 11 },
+                new int[] { 4, 9, 9, 49, 121 }
+            }
+        };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(int[] nums, int[] expected)
+        {
+            Solution solution = new();
+            var actual = solution.SortedSquares(nums);
+            Assert.That(actual.Length, Is.EqualTo(expected.Length));
+            Assert.That(actual.SequenceEqual(expected));
         }
     }
 }
