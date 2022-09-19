4b8ca29e69a935922f5dcd26942b0ea6b7e726db
stormholder <dantedmc78@gmail.com>
2022-09-19 20:37:03 +0300

    Update: added tesk 2 solution

--- 189_Rotate_Array.Test/UnitTest1.cs
+++ 189_Rotate_Array.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using RotateArray;
+
 namespace _189_Rotate_Array.Test
 {
     public class Tests
@@ -7,10 +9,28 @@ namespace _189_Rotate_Array.Test
         {
         }
 
-        [Test]
-        public void Test1()
+        static object[] TestCases =
         {
-            Assert.Pass();
+            new object[]
+            {
+                new int[] { 1, 2, 3, 4, 5, 6, 7 },
+                3,
+                new int[] { 5, 6, 7, 1, 2, 3, 4 }
+            },
+            new object[]
+            {
+                new int[] { -1, -100, 3, 99 },
+                2,
+                new int[] { 3, 99, -1, -100 }
+            }
+        };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(int[] nums, int k, int[] expected)
+        {
+            Solution solution = new();
+            solution.Rotate(nums, k);
+            Assert.That(nums.SequenceEqual(expected));
         }
     }
 }
--- 189_Rotate_Array/Program.cs
+++ 189_Rotate_Array/Program.cs
@@ -4,7 +4,26 @@ public class Solution
 {
     public void Rotate(int[] nums, int k)
     {
-        //
+        int[] tmp = new int[k];
+        int[] tmp2 = new int[nums.Length - k];
+        int i = 0;
+        for (; i < nums.Length - k; i++)
+        {
+            tmp2[i] = nums[i];
+        }
+        for (int j = 0; i < nums.Length; i++, j++)
+        {
+            tmp[j] = nums[i];
+        }
+
+        for (int j = 0; j < tmp.Length; j++)
+        {
+            nums[j] = tmp[j];
+        }
+        for (int j = 0; j < tmp2.Length; j++)
+        {
+            nums[j + k] = tmp2[j];
+        }
     }
 }
 internal class Program
