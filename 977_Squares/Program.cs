6d1c5fa2bfbb6ee47f43d2e7c65bbd6e9acf90c0
stormholder <dantedmc78@gmail.com>
2022-09-19 18:55:09 +0300

    Update: task 1 easy solution

--- 977_Squares/Program.cs
+++ 977_Squares/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Squares;
+public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = nums[i] * nums[i];
+        }
+        Array.Sort(result);
+        return result;
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
