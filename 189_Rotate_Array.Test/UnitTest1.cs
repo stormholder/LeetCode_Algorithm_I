a2efaed9f67be598b51ba062d27d29281f615d1f
stormholder <dantedmc78@gmail.com>
2022-09-19 18:16:29 +0300

    Add Day 2 task 2

---
+++ 189_Rotate_Array.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace _189_Rotate_Array.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
