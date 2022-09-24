c36a406f7631ed8220eacad7aafcee6667bf3361
stormholder <dantedmc78@gmail.com>
2022-09-24 19:48:47 +0300

    Update: task#344 - solution

--- 344_Reverse_String/Program.cs
+++ 344_Reverse_String/Program.cs
@@ -4,7 +4,21 @@ public class Solution
 {
     public void ReverseString(char[] s)
     {
-
+        bool isOdd = s.Length % 2 == 0;
+        int il = isOdd ? s.Length / 2 - 1 : s.Length / 2;
+        int ir = isOdd ? il + 1 : il;
+        char tmp;
+        while (il >= 0)
+        {
+            if (s[il] != s[ir])
+            {
+                tmp = s[il];
+                s[il] = s[ir];
+                s[ir] = tmp;
+            }
+            il--;
+            ir++;
+        }
     }
 }
 
