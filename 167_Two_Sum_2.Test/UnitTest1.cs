fbdb9ef40de919357969ce7712457db590f2b92f
stormholder <dantedmc78@gmail.com>
2022-09-24 18:07:15 +0300

    Update: task#167 - added naive solution; Update: task#167 - added README

--- 167_Two_Sum_2.Test/UnitTest1.cs
+++ 167_Two_Sum_2.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using TwoSum2;
+
 namespace _167_Two_Sum_2.Test
 {
     public class Tests
@@ -7,10 +9,35 @@ namespace _167_Two_Sum_2.Test
         {
         }
 
-        [Test]
-        public void Test1()
+        static object[] TestCases =
         {
-            Assert.Pass();
+            new object[]
+            {
+                new int[] { 2, 7, 11, 15 },
+                9,
+                new int[] { 1, 2 }
+            },
+            new object[]
+            {
+                new int[] { 2, 3, 4 },
+                6,
+                new int[] { 1, 3 }
+            },
+            new object[]
+            {
+                new int[] { -1, 0 },
+                -1,
+                new int[] { 1, 2 }
+            },
+        };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(int[] numbers, int target, int[] expected)
+        {
+            Solution solution = new();
+            var actual = solution.TwoSum(numbers, target);
+            Console.WriteLine( $"{string.Join(',', actual)} =?= {string.Join(',', expected)}" );
+            Assert.That(actual.SequenceEqual(expected));
         }
     }
 }
--- 167_Two_Sum_2/Program.cs
+++ 167_Two_Sum_2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TwoSum2;
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int p = 0;
+        int i = 1;
+        while(i < numbers.Length)
+        {
+            int a = numbers[p];
+            int delta = target - a;
+            int deltaIndex = -1;
+            for (int j = p; j < numbers.Length; j++)
+            {
+                if (numbers[j] == delta)
+                {
+                    deltaIndex = j;
+                    break;
+                }
+            }
+            if (deltaIndex < 0)
+            {
+                p++;
+            }
+            else
+            {
+                i = deltaIndex;
+                break;
+            }
+            i++;
+        }
+
+        return new int[2] { p + 1, i + 1 };
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
