01b5d02d85f3877cf0dd89a4a09ef220acb6a511
stormholder <dantedmc78@gmail.com>
2022-09-25 11:54:46 +0300

    Update: debugging task#19

--- 19_Remove_Nth_Node/Program.cs
+++ 19_Remove_Nth_Node/Program.cs
@@ -13,11 +13,47 @@ public class ListNode
 
 public class Solution
 {
+    private int Count(int counter, ListNode head)
+    {
+        if (head == null)
+            return counter;
+        return Count(counter + 1, head.next);
+    }
+
     public ListNode RemoveNthFromEnd(ListNode head, int n)
     {
         ListNode result = new();
+        int length = Count(0, head);
+        int toRemove = length - n;
+        Console.WriteLine($"Remove {toRemove}th element");
+        int[] prefixVals = new int[toRemove];
+        int[] suffixVals = new int[n - 1];
+        ListNode node = head;
+        if (toRemove > 0)
+        {
+            for (int i = 0; i < toRemove; i++)
+            {
+                prefixVals[i] = node.val;
+                node = node.next;
+            }
+        }
+        node = node.next;
+        if (n - 1 > 0)
+        {
+            int j = 0;
+            while (node != null || j < n - 2)
+            {
+                suffixVals[j] = node.val;
+                j++;
+                node = node.next;
+            }
+        }
+        Console.WriteLine($"Prefixes: {string.Join(',', prefixVals)}");
+        Console.WriteLine($"Suffixes: {string.Join(',', suffixVals)}");
+
         return result;
     }
+
     public List<int> ToList(ref List<int> list, ListNode head)
     {
         if (head == null)
