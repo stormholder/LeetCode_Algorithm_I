634637fad8a41db9ca8e3b5e929e9db6ff067113
stormholder <dantedmc78@gmail.com>
2022-09-26 19:10:22 +0300

    Add: Day 7 tasks #733, #695

---
+++ 695_Max_Island/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MaxIsland;
+
+public class Solution
+{
+    public int MaxAreaOfIsland(int[][] grid)
+    {
+
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
---
+++ 733_Flood_Fill/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FloodFill;
+
+public class Solution
+{
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
