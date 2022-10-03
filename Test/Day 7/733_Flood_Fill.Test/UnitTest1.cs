cea30ca82da22084dccd8613db1428528630f99d
stormholder <dantedmc78@gmail.com>
2022-10-03 13:51:08 +0300

    Update: day 7 task#733 - added unit test cases

--- Test/Day 7/733_Flood_Fill.Test/UnitTest1.cs
+++ Test/Day 7/733_Flood_Fill.Test/UnitTest1.cs
@@ -1,16 +1,54 @@
+using FloodFill;
+
 namespace _733_Flood_Fill.Test
 {
     public class Tests
     {
+        static object[] TestCases =
+        {
+            new object[]
+            {
+                new int[][] {
+                    new int[] { 1, 1, 1 },
+                    new int[] { 1, 1, 0 },
+                    new int[] { 1, 0, 1 }
+                },
+                1,
+                1,
+                2,
+                new int[][] {
+                    new int[] { 2, 2, 2 },
+                    new int[] { 2, 2, 0 },
+                    new int[] { 2, 0, 1 }
+                }
+            },
+            new object[]
+            {
+                new int[][] {
+                    new int[] { 0, 0, 0 },
+                    new int[] { 0, 0, 0 }
+                },
+                0,
+                0,
+                0,
+                new int[][] {
+                    new int[] { 0, 0, 0 },
+                    new int[] { 0, 0, 0 }
+                }
+            }
+        };
+
         [SetUp]
         public void Setup()
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(int[][] image, int sr, int sc, int color, int[][] expected)
         {
-            Assert.Pass();
+            Solution solution = new();
+            var actual = solution.FloodFill(image, sr, sc, color);
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
