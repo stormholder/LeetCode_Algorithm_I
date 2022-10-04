bcd8748f9b2038211539a6ae776eb45964b2a04c
stormholder <dantedmc78@gmail.com>
2022-10-04 20:36:50 +0300

    Update: day 8 task#617 - adde unit tests + refreshed boilerplate code

--- Src/Day 8/617_MergeTrees/Program.cs
+++ Src/Day 8/617_MergeTrees/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MergeTrees;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+public class Solution
+{
+    public TreeNode ArrayToTreeNode(int?[] root)
+    {
+        // TODO
+        return new TreeNode();
+    }
+
+    public int?[] TreeNodeToArray(TreeNode root)
+    {
+        // TODO
+        return new int?[] { null };
+    }
+
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+    {
+        return new TreeNode();
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
--- Test/Day 8/617_MergeTrees.Test/UnitTest1.cs
+++ Test/Day 8/617_MergeTrees.Test/UnitTest1.cs
@@ -1,16 +1,36 @@
+using MergeTrees;
+
 namespace _617_MergeTrees.Test
 {
     public class Tests
     {
+        static object[] TestCases =
+        {
+            new object[]
+            {
+                new int?[] { 1, 3, 2, 5 },
+                new int?[] { 2, 1, 3, null, 4, null, 7 },
+                new int?[] { 3, 4, 5, 5, 4, null, 7 }
+            },
+            new object[]
+            {
+                new int?[] { 1 },
+                new int?[] { 1, 2 },
+                new int?[] { 2, 2 }
+            }
+        };
+
         [SetUp]
         public void Setup()
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(int?[] root1, int?[] root2, int?[] expected)
         {
-            Assert.Pass();
+            Solution solution = new();
+            var actual = solution.MergeTrees(solution.ArrayToTreeNode(root1), solution.ArrayToTreeNode(root2));
+            Assert.That(solution.TreeNodeToArray(actual), Is.EqualTo(expected));
         }
     }
 }
