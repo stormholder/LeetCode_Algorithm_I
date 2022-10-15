1c73994323056bcbcff9271cc5014d87978342d1
stormbringer <dantedmc78@gmail.com>
2022-10-15 12:08:12 +0300

    Update: day 9 task#542 - small optimisation

--- Src/Day 9/542_Matrix/Program.cs
+++ Src/Day 9/542_Matrix/Program.cs
@@ -1,11 +1,5 @@
 ﻿namespace Matrix;
 
-public struct Tile
-{
-    public int X {get;set;}
-    public int Y {get;set;}
-}
-
 public class Solution
 {
     private readonly int[,] DIRECTIONS = new int[,]{{-1,0},{1,0},{0,-1},{0,1}};
@@ -20,7 +14,7 @@ public class Solution
             result[y] = new int[xMax];
             Array.Fill(result[y], int.MaxValue - 1);
         }
-        Queue<Tile> queue = new();
+        Queue<int[]> queue = new();
         for (int y = 0; y < yMax; y++)
         {
             for (int x = 0; x < xMax; x++)
@@ -28,25 +22,25 @@ public class Solution
                 if (mat[y][x] == 0)
                 {
                     result[y][x] = 0;
-                    queue.Enqueue(new Tile() { X = x, Y = y });
+                    queue.Enqueue(new int[2] { x, y });
                 }
             }
         }
 
         while (queue.Count > 0)
         {
-            Tile tile = queue.Dequeue();
+            var tile = queue.Dequeue();
             for (int i = 0; i < 4; i++)
             {
-                int new_row = tile.Y + DIRECTIONS[i, 0];
-                int new_col = tile.X + DIRECTIONS[i, 1];
+                int new_col = tile[0] + DIRECTIONS[i, 1];
+                int new_row = tile[1] + DIRECTIONS[i, 0];
                 
                 if(new_row >= 0 && new_col >= 0 && new_row < yMax && new_col < xMax)
                 {
-                    if(result[new_row][new_col] > result[tile.Y][tile.X] + 1)
+                    if(result[new_row][new_col] > result[tile[1]][tile[0]] + 1)
                     {
-                        result[new_row][new_col] = result[tile.Y][tile.X] + 1;
-                        queue.Enqueue(new Tile() { Y = new_row, X = new_col });
+                        result[new_row][new_col] = result[tile[1]][tile[0]] + 1;
+                        queue.Enqueue(new int[2] { new_col, new_row });
                     }
                 }
             }
